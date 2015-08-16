82eee3b8d9a5ef83ddeade365fc6a179f0e0fbe9
sbluen <sbluen153@gmail.com>
2015-08-15 22:32:47 -0700

    -write implementation -add sample files

--- main.py
+++ main.py
@@ -6,8 +6,32 @@ Created on Aug 15, 2015
 
 import os
 import csv
+import re
+
+class_template = """public class {classname} {{
+    public {classname}({args}) {{
+        {inits}
+    }}
+}}
+"""
 
 for filename in os.listdir("inputs"):
+    
     if not filename.endswith(".csv"):
         continue
-    java_filename = ".".join(filename.split(".")[:-1]) + ".java"
+    
+    reader = csv.DictReader(file(os.path.join("inputs", filename)))
+    fieldnames = reader.fieldnames
+    fieldnames = [fieldname.replace(" ", "_") for fieldname in fieldnames]
+    fieldnames = [re.sub("[^a-zA-Z_]", "", fieldname) for fieldname in fieldnames]
+    
+    classname = ".".join(filename.split(".")[:-1])
+    args = ", ".join(["String %s" % i for i in fieldnames])
+    inits = "\n        ".join(["this.%s = %s;" % (i, i) for i in fieldnames])
+    output_text = class_template.format(classname=classname, args=args, inits=inits) 
+    java_filename_path = os.path.join("outputs", classname + ".java")
+    
+    with file(java_filename_path, "w") as f:
+        f.write(output_text)
+    
+print("done")
---
+++ outputs/sample.java
@@ -0,0 +1,8 @@
+public class sample {
+    public sample(String a, String b, String cd, String e_f) {
+        this.a = a;
+        this.b = b;
+        this.cd = cd;
+        this.e_f = e_f;
+    }
+}
