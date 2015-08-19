ed2d3fd30d88ca4aa89d55f44e424cc854df1693
sbluen <sbluen153@yahoo.com>
2015-08-19 08:45:11 -0700

    Update main.py
    update and include declarations

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ import csv
 import re
 
 class_template = """public class {classname} {{
+    {declarations}
+    
     public {classname}({args}) {{
         {inits}
     }}
     
-    public {classname}(List source) {{
+    public {classname}(Map<String, String> source) {{
         {arraylist_inits}
     }}
     
@@ -22,20 +24,28 @@ class_template = """public class {classname} {{
 }}
 """
 
+
+print("To be pasted into spreadsheets:")
 for filename in os.listdir("inputs"):
     
-    if not filename.endswith(".csv"):
+    if (not filename.endswith(".csv") or 
+        not filename.count(".") == 1 or 
+        not re.match("([a-zA-Z_$][a-zA-Z\d_$]*)*[a-zA-Z_$][a-zA-Z\d_$]*", filename)):
+        print("Cannot generate valid class name from %s" % filename)
         continue
     
     reader = csv.DictReader(file(os.path.join("inputs", filename)))
     fieldnames = reader.fieldnames
-    fieldnames = [fieldname.replace(" ", "_") for fieldname in fieldnames]
-    fieldnames = [re.sub("[^a-zA-Z_]", "", fieldname) for fieldname in fieldnames]
+    original_fieldname_values = fieldnames
+    fieldnames = [re.sub("[^a-zA-Z_ ]", "", fieldname) for fieldname in fieldnames]
+    fieldnames = [fieldname.strip().replace(" ", "_") for fieldname in fieldnames]
+    original_fieldnames = dict(zip(fieldnames, original_fieldname_values))
     
     classname = filename[0].upper() + filename.split(".")[0][1:]
+    declarations = "\n    ".join(["private String %s;" % i for i in fieldnames])
     args = ", ".join(["String %s" % i for i in fieldnames])
     inits = "\n        ".join(["this.%s = %s;" % (i, i) for i in fieldnames])
-    arraylist_inits = "\n        ".join(["this.%s = source.get(%s);" % (i, i) for i in fieldnames]) 
+    arraylist_inits = "\n        ".join(["this.%s = source.get(\"%s\");" % (i, original_fieldnames[i]) for i in fieldnames]) 
     
     getters = ""
     setters = ""
@@ -46,7 +56,8 @@ for filename in os.listdir("inputs"):
         this.{fieldname} = {fieldname};
     }}
     
-    """.format(cased_name=cased_name, fieldname=fieldname)
+    """.format(cased_name=cased_name, fieldname=fieldname, 
+               original_fieldname = original_fieldnames[fieldname])
     
         setters += """    public String get{cased_name}() {{
         return {fieldname};
@@ -56,10 +67,15 @@ for filename in os.listdir("inputs"):
     
     output_text = class_template.format(classname=classname, args=args,
         inits=inits, arraylist_inits=arraylist_inits,
-        getters=getters, setters=setters)
+        getters=getters, setters=setters, declarations=declarations)
     
     java_filename_path = os.path.join("outputs", classname + ".java")
     with file(java_filename_path, "w") as f:
         f.write(output_text)
     
+    stdout_text = filename.split(".")[0] + ": "
+    for fieldname in fieldnames:
+        stdout_text += "${value.%s}\t" % fieldname
+    stdout_text = stdout_text[:-1] #drop the last tab char
+    print(stdout_text)
 print("done")
